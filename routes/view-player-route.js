8972332036a564e2e45ad624ab0e12963a83f380
DimitriosKan <mrhydro7k@gmail.com>
2019-07-02 17:27:09 +0100

    Player viewing setup

--- routes/view-player-route.js
+++ routes/view-player-route.js
@@ -1,6 +1,8 @@
-var express = require("Express");
+var express = require('express');
 var router = express.Router();
-var Player = require("../schemas/player-schema");
+var Player = require('../schemas/player-schema');
+var param = require('../handler/param-handler');
+var mongoose = require('mongoose');
 
 router.get('/allUsers', (req, res) => {
   return Player.PlayerModel.find().then((doc) => {
@@ -8,10 +10,14 @@ router.get('/allUsers', (req, res) => {
   });
 });
 
-router.get('/byUsername', (req, res) => {
-  return Player.PlayerModel.find({ username }).then((doc) => {
-    return res.send(doc);
-  });
+router.get('/byUsername',
+  (req, res, next) => param(req, res, ['username'], next),
+  (req, res) => {
+    const { username } = req.query;
+    return Player.PlayerModel.find({ username }).then(
+      doc => res.send(doc),
+      error => res.send(500).send(error)
+    );
 });
 
 module.exports = router;
