5a9328b78b20862a7f0995d3db482141ab897a72
Jaydeep Hasmukhlal <jaydeep.hasmukhlal@academytrainee.com>
2019-07-02 10:32:23 +0100

    Created schema and working on routes.

---
+++ routes/match-routes.js
@@ -0,0 +1,4 @@
+var express = require("Express");
+var router = express.Router();
+var Matches = require("../schemas/matches-schema");
+
---
+++ schemas/matches-schema.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+
+exports.MatchesSchema = mongoose.Schema({
+    player1 : {
+        type : Number,
+        require : true
+    },
+    player2 : {
+        type : Number,
+        require : true
+    },
+    player1_win : Boolean,
+    player1_lose : Boolean,
+    player2_win : Boolean,
+    player2_lose : Boolean,
+    player1_7balled : Boolean,
+    player2_7balled : Boolean,
+    timestamp : true,
+    created_match : {
+        type: Date,
+        require : true
+    },
+    finished_match : Date
+});
+
+exports.MatchesModel = mongoose.model("Matches"), this.MatchesSchema;
