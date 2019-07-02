96c857654b754b8049b0981cd096b04fbe2ded9f
Hugh <hughfharding@gmail.com>
2019-07-02 12:31:46 +0100

    added dummy function getMatch

--- routes/match-routes.js
+++ routes/match-routes.js
@@ -2,11 +2,18 @@ var express = require("Express");
 var router = express.Router();
 var matches = require("../schemas/matches-schema");
 
-router.get("/matches", async (req, res) => {
+router.get("/getMatches", async (req, res) => {
     const matches = matches.MatchesModel.find().then((doc) => {
         return res.send(doc);
     });
 });
+
+router.get("/getMatch", (req, res) => {
+    let thePlayer = "";
+
+    let id = PlayerSchema.find({player: thePlayer}, player);
+});    
+
 
 router.post("/createMatch", (req, res, next) => {
     const newMatch = new matches.MatchesModel(req.body);
@@ -15,3 +22,7 @@ router.post("/createMatch", (req, res, next) => {
         error => next(error)
     );
 });
+
+
+
+
