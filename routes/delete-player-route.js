370eb734ff0a846839451a889208391701d78fd9
Johnny <Jonathan.PeckoverQA@academytrainee.com>
2019-07-02 14:40:56 +0100

    housekeeping

--- routes/delete-player-route.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var Player = require('../schemas/player-schema');
-
-router.delete('/deletePlayer', (req, res, ['username'], next) => {
-    
-})
