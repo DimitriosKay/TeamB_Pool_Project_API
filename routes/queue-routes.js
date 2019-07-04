427f3aaf72428feccb1c57df4e43a79a58ce25c3
Ferdinand-Oluwaseye <ronferd2012@gmail.com>
2019-07-04 01:25:06 +0100

    Added queue routes and crud functions and edited the schema to remove the queueid

--- routes/queue-routes.js
+++ routes/queue-routes.js
@@ -1,3 +1,40 @@
-var express = require("Express");
+var express = require('express');
 var router = express.Router();
-var Matches = require("../schemas/queue-schema");
+var Queues = require('../schemas/queue-schema');
+var paramHandler = require('../handlers/param-handler');
+var utils = require('../utils');
+
+
+router.get('/all', (req, res) => {
+    return Queues.queueModel.find().then((doc) => {
+        return res.send(doc);
+    });
+});
+
+
+router.post('/createQueue', (req, res, next) => {
+    const newQueue = new Queues.queueModel.(req.body);
+    return newQueue.save().then(
+        doc => res.status(201).send(doc),
+        error => next(error)
+    );
+});
+
+router.delete('/deleteQueue',
+    (req, res, next) => paramHandler(req, res, ['username'], next),
+    async () => {
+        try {
+            const username = req.query.username;
+            try {
+                await Player.PlayerModel.findAndDelete(username);
+                res.status(204).send();
+            } catch (exc) {
+                next(exc);
+            }
+        } catch (exc) {
+            next({ message: exc.message });
+            return;
+        }
+    });
+
+    module.exports = router;
